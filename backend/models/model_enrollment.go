ce5c44bfb76ed2fd2dd77ee128754b5b19b42fc3
geronimo crostelli <88061632+Geronimo2001@users.noreply.github.com>
2024-06-07 17:01:38 -0300

    Delete backend directory

--- backend/models/model_enrollment.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "time"
-
-type Enrollment struct {
-	UserID   uint      `gorm:"foreignKey:ID"`
-	CourseID uint      `gorm:"foreignKey:ID"`
-	Date     time.Time `gorm:"type:datetime"`
-}
